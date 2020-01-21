7935fb0acca811ca17db1af7caa8ae81a50a4f30
Deanna Kelley <deanna.georges@gmail.com>
2020-01-21 18:51:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,13 +34,12 @@ class App < Sinatra::Base
     case @operation
       when "add"
         @number1 + @number2
-<% when "subtract" %>
-<%= @number1 - @number2 %>
-<% when "multiply" %>
-<%= @number1 * @number2 %>
-<% when "divide" %>
-<%= @number1 / @number2 %>
-<% end %>
-    
+      when "subtract"
+        @number1 - @number2
+      when "multiply"
+        @number1 * @number2
+      when "divide"
+        @number1 / @number2
+      end
   end 
 end
